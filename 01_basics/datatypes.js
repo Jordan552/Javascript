d6c533234e3823791ca5bc19cd6efa4fe089f62d
Awanish Kumar <115667187+Jordan552@users.noreply.github.com>
2024-07-23 09:22:46 +0000

    data types added

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,35 @@
+// primitive
+
+//7 types : string, number, boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+
+const bigNumber = 1477897807731773817388n
+
+
+
+//Refrence (non primitive)
+
+// array, objects, functions
+
+const heros = ["shaktiman", "spiderman", "dodo"];
+let myObj = {
+    name :"Jordan",
+    age: 27,
+}
+
+const MyFunction = function(){
+    console.log();
+}
