5e413d6f0d5b235d49ee5f3d47035960b22b737f
Awanish Kumar <115667187+Jordan552@users.noreply.github.com>
2024-07-25 21:58:39 +0000

    date

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,10 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+
+let myCreateDate = new Date(2024,0,26)
+console.log(myCreateDate.toString());
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
-console.log("jordan")
+let date = new Date();
+
+date.setFullYear(2025);
+date.setMonth(11); // 11 = December
+date.setDate(31);
+date.setHours(23);
+date.setMinutes(59);
+date.setSeconds(59);
+date.setMilliseconds(999);
+
+console.log(date);
